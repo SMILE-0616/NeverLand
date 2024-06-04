c221fef912ac5de26ac6217bf1609654c9f95819
SMILE <ojh06160616@gmail.com>
2024-06-05 03:54:51 +0900

    commit 3

---
+++ Assets/2Scripts/SceneMoveBack.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SceneMoveBack : MonoBehaviour
+{
+    public void GameSceneCtrl1()
+    {
+        SceneManager.LoadScene("Scene 0");
+    }
+    public void GameSceneCtrl2()
+    {
+        SceneManager.LoadScene("Scene 1");
+    }
+    public void GameSceneCtrl3()
+    {
+        SceneManager.LoadScene("Scene 2");
+    }
+    public void GameSceneCtrl4()
+    {
+        SceneManager.LoadScene("Scene 3");
+    }
+    public void GameSceneCtrl5()
+    {
+        SceneManager.LoadScene("Scene 4");
+    }
+    public void GameSceneCtrl6()
+    {
+        SceneManager.LoadScene("Scene 5");
+    }
+    public void GameSceneCtrl7()
+    {
+        SceneManager.LoadScene("Scene 6");
+    }
+    public void GameSceneCtrl8()
+    {
+        SceneManager.LoadScene("Scene 7");
+    }
+    public void GameSceneCtrl9()
+    {
+        SceneManager.LoadScene("Scene 8");
+    }
+    public void GameSceneCtrl10()
+    {
+        SceneManager.LoadScene("Scene 9");
+    }
+    public void GameSceneCtrl11()
+    {
+        SceneManager.LoadScene("Scene 10");
+    }
+    public void GameSceneCtrl12()
+    {
+        SceneManager.LoadScene("Scene 11");
+    }
+}
